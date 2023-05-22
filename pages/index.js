17225c5fe49cff8e6acb5eeac66562132883ddb4
masac <msktjm98@gmail.com>
2023-05-22 19:06:19 +0900

    目次作成

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Top from './Top';
+import Link from 'next/link';
 
 const Home = () => {
   return (
@@ -10,10 +10,48 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <h1>Hello, Next.js !!</h1>
-      </div>
-      {/* <Top /> */}
+      <ul>
+        <li>
+          <Link href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/logIn">
+            login
+          </Link>
+        </li>
+        <li>
+          <Link href="/signUp">
+            signUp
+          </Link>
+        </li>
+        <li>
+          <Link href="/top">
+            top
+          </Link>
+        </li>
+        <li>
+          <Link href="/create">
+            create
+          </Link>
+        </li>
+        <li>
+          <Link href="/edittodo">
+            editTodo
+          </Link>
+        </li>
+        <li>
+          <Link href="/todoShow">
+            todoShow
+          </Link>
+        </li>
+        <li>
+          <Link href="/sdfglsdrg">
+            404
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
