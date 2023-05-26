53f959102a4da8d8857572af1466471cca0f19ef
K <ken0127hotmilk@gmail.com>
2023-05-26 11:21:12 +0900

    #15feature/update first-commit

--- pages/edittodo.js
+++ pages/edittodo.js
@@ -1,123 +1,139 @@
-import { Box,
-    Button,
-    Flex,
-    FormControl,
-    FormLabel,
-    Input,
-    Spacer,
-    Heading,
-    Textarea,
-    Text,
-    Container } from '@chakra-ui/react';
-import Head from 'next/head'
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Spacer,
+  Heading,
+  Textarea,
+  Text,
+  Container,
+} from "@chakra-ui/react";
+// import Head from 'next/head'
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { db } from "@/libs/firebase";
+import { useEffect, useState } from "react";
 
-const EditTodo =()=>{
+const EditTodo = () => {
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("編集したいdetail");
 
-    return(
-        <>
-            <Box w='100%'
-                h='80px'
-                bg='green.300'
-                display='flex'
-                justifyContent='center'
-                alignItems='center'
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleChangeDetail = (e) => {
+    setDetail(e.target.value);
+  };
+
+  const document = "1";
+  //↑ドキュメントをfirebaseから取得して入れ込む
+  //選択したtodoのidか何かとヒモづける必要がある
+
+  useEffect(() => {
+    const fetchData = async () => {
+  const docRef = doc(db, "todos", document);
+  const docSnap = await getDoc(docRef);
+  setTitle(docSnap.data().title);
+  setDetail(docSnap.data().detail)
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Box
+        w="100%"
+        h="80px"
+        bg="green.300"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Heading
+          w="85%"
+          maxW="1080px"
+          color="blackAlpha.800"
+          fontSize="48px"
+          fontWeight="bold"
+        >
+          TODO
+        </Heading>
+      </Box>
+
+      <Container w="85%" maxW="1080px" pt="16px">
+        <Box>
+          <Flex w={"100%"}>
+            <Heading as="h2" fontSize="28px" fontWeight="bold">
+              EDIT TODO
+            </Heading>
+            <Spacer />
+            <Button
+              w={28}
+              bg="green.300"
+              rounded="full"
+              borderWidth="1px"
+              borderColor="blackAlpha.800"
             >
-                <Heading
-                    w='85%'
-                    maxW='1080px'
-                    color='blackAlpha.800'
-                    fontSize='48px'
-                    fontWeight='bold'
-                >
-                TODO
-                </Heading>
-            </Box>
+              Back
+            </Button>
+          </Flex>
+          <Box w="100%">
+            <FormControl mb="20px">
+              <FormLabel fontSize="24px" fontWeight="bold" lineHeight={1}>
+                TITLE
+              </FormLabel>
+              <Input
+                value={title}
+                onChange={handleChangeTitle}
+                h="70px"
+                fontSize="20px"
+                rounded="10px"
+                type="text"
+                borderColor="blackAlpha.800"
+                // placeholder='Github上に静的サイトをホスティングする'
+              />
+            </FormControl>
+            <FormControl mb="20px">
+              <FormLabel fontSize="24px" fontWeight="bold" lineHeight={1}>
+                DETAIL
+              </FormLabel>
+              <Textarea
+                h="320px"
+                rounded="10px"
+                borderColor="blackAlpha.800"
+                fontSize="20px"
+                value={detail}
+                onChange={handleChangeDetail}
+              ></Textarea>
+            </FormControl>
 
-            <Container
-                w='85%'
-                maxW='1080px'
-                pt='16px'
-            >
-                <Box>
-                    <Flex w={'100%'}>
-                        <Heading as='h2'
-                            fontSize='28px'
-                            fontWeight='bold'
-                        >
-                            EDIT TODO
-                        </Heading>
-                        <Spacer />
-                        <Button
-                            w={28}
-                            bg='green.300'
-                            rounded='full'
-                            borderWidth='1px'
-                            borderColor='blackAlpha.800'
-                        >
-                            Back
-                        </Button>
-                    </Flex>
-                    <Box w='100%'>
-                        <FormControl mb='20px'>
-                            <FormLabel
-                                fontSize='24px'
-                                fontWeight='bold'
-                                lineHeight={1}
-                            >
-                                TITLE
-                            </FormLabel>
-                            <Input
-                                h='70px'
-                                fontSize='20px'
-                                rounded='10px'
-                                type='text'
-                                borderColor='blackAlpha.800'
-                                placeholder='Github上に静的サイトをホスティングする' 
-                            />
-                        </FormControl>
-                        <FormControl mb='20px'>
-                            <FormLabel
-                                fontSize='24px'
-                                fontWeight='bold'
-                                lineHeight={1}
-                            >
-                                DETAIL
-                            </FormLabel>
-                            <Textarea
-                                h='320px'
-                                rounded='10px'
-                                borderColor='blackAlpha.800'
-                                fontSize='20px'
-                                placeholder='AWS コンソールで AWS Amplify を使って静的ウェブサイトをホスティングします。AWS Amplify は、静的ウェブサイトおよびウェブアプリにフルマネージドのホスティングを提供します。Amplify のホスティングソリューションは、Amazon CloudFront と Amazon S3 を使って、AWS コンテンツ配信ネットワーク (CDN) を介してサイトアセットを提供します。'
-                            >
-                            </Textarea>
-                        </FormControl>
-
-                        <Flex mt={1}>
-                            <Flex direction ='column' mr={27}>
-                                <Text>Create</Text>
-                                <Text fontSize='20px'>2020-11-8 18:55</Text>
-                            </Flex>
-                            <Flex direction ='column'>
-                                <Text>Update</Text>
-                                <Text fontSize='20px'>2020-11-8 18:55</Text>
-                            </Flex>
-                            <Spacer />
-                            <Button
-                                w={28}
-                                bg='green.600'
-                                rounded='full'
-                                color='green.50'
-                                borderWidth='1px'
-                                borderColor='blackAlpha.800'
-                            >
-                                UPDATE
-                            </Button>
-                        </Flex>
-                    </Box>
-                </Box>
-            </Container>
-        </>
-    )
-}
+            <Flex mt={1}>
+              <Flex direction="column" mr={27}>
+                <Text>Create</Text>
+                <Text fontSize="20px">2020-11-8 18:55</Text>
+              </Flex>
+              <Flex direction="column">
+                <Text>Update</Text>
+                <Text fontSize="20px">2020-11-8 18:55</Text>
+              </Flex>
+              <Spacer />
+              <Button
+                w={28}
+                bg="green.600"
+                rounded="full"
+                color="green.50"
+                borderWidth="1px"
+                borderColor="blackAlpha.800"
+              >
+                UPDATE
+              </Button>
+            </Flex>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
 export default EditTodo;
