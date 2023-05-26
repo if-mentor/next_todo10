de9d06af73a49eaa55347ecd04cc46cbea71ab43
sei693 <croissant.moimoi@gmail.com>
2023-05-27 01:40:23 +0900

    #18feature/auth 作成

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useState } from "react";
+import { useRouter } from "next/router";
 import {
   Box,
   Button,
@@ -6,8 +8,43 @@ import {
   Input,
   Heading,
 } from "@chakra-ui/react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../libs/firebase";
 
-const login = () => {
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+    ...formData,
+    [e.target.id]: e.target.value,
+    });
+  };
+
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    if (userCredential.user) {
+      router.push("/top")
+    }
+    } catch (error) {
+      //エラー時にはポップアップかバリデーションエラーを適用したい
+    console.log(error);
+    }
+  };
+  
   return (
     <>
       <Heading w={"1280px"} h={"80px"} bg="#68D391">
@@ -29,16 +66,32 @@ const login = () => {
 
             <Box mt={"24px"} padding={"0px 50px"} >
               <Text fontWeight="bold" fontStyle={'Gothic A1'}>メールアドレス</Text>
-              <Input bg={"green.50"} borderRadius={"40px"} />
+              <Input
+                type="email"
+                placeholder="Email"
+                id="email"
+                value={email}
+                required
+                onChange={(e)=>onChange(e)}
+                bg={"green.50"} borderRadius={"40px"}
+              />
             </Box>
 
             <Box mt={"24px"} padding={"0px 50px"}>
               <Text fontWeight="bold" fontStyle={'Gothic A1'}>パスワード</Text>
-              <Input bg={"green.50"} borderRadius={"40px"}/>
+              <Input
+                type="password"
+                placeholder="Password"
+                id="password"
+                value={password}
+                required
+                onChange={(e)=>onChange(e)}
+                bg={"green.50"} borderRadius={"40px"}
+              />
             </Box>
 
             <Box textAlign={"center"}>
-              <Button display={"inline-block"} mt={"24px"} color={"white"} bg={"green.600"} borderRadius={"50px"} height={"60px"} width={"200px"}>
+              <Button onClick={(e)=>onSubmit(e)} display={"inline-block"} mt={"24px"} color={"white"} bg={"green.600"} borderRadius={"50px"} height={"60px"} width={"200px"}>
                 LOGIN
               </Button>
             </Box>
@@ -46,9 +99,8 @@ const login = () => {
         </Box>
       </Box>
 
-
     </>
   )
 }
 
-export default login
+export default Login
--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useState } from 'react'
+import { useRouter } from "next/router";
 import {
   Box,
   Button,
@@ -6,8 +8,53 @@ import {
   Input,
   Heading,
 } from "@chakra-ui/react";
+import {
+  updateProfile,
+  createUserWithEmailAndPassword,
+  } from "firebase/auth";
+  import { auth } from "../libs/firebase";
 
-const signup = () => {
+
+const  Signup = () => {
+  //入力したデータをまとめる
+  const [formData,setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+    ...formData,
+    [e.target.id]: e.target.value,
+    });
+  }
+
+  //ページ遷移させるためにuseRouterを定義
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    //コンソールで機能してるかを確認
+    // console.log("Signup OK!")
+    e.preventDefault();
+    try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    // データベースに登録する
+    updateProfile(auth.currentUser, {displayName: name,}
+    );
+    //topにページ遷移する
+      router.push("/top")
+    } catch (error) {
+      //エラー時にはポップアップかバリデーションエラーを適用したい
+    console.log(error);
+    }
+  };
+  
+
   return (
     <>
       <Heading w={"1280px"} h={"80px"} bg="#68D391">
@@ -29,26 +76,39 @@ const signup = () => {
 
             <Box mt={"24px"} padding={"0px 50px"} >
               <Text fontWeight="bold" fontStyle={'Gothic A1'}>メールアドレス</Text>
-              <Input bg={"green.50"} borderRadius={"40px"} />
+              <Input
+                type="email"
+                placeholder="Email"
+                id="email"
+                value={email}
+                required
+                onChange={(e)=>onChange(e)}
+                bg={"green.50"} borderRadius={"40px"} />
             </Box>
 
             <Box mt={"24px"} padding={"0px 50px"}>
               <Text fontWeight="bold" fontStyle={'Gothic A1'}>パスワード</Text>
-              <Input bg={"green.50"} borderRadius={"40px"}/>
+              <Input
+                type="password"
+                placeholder="Password"
+                id="password"
+                value={password}
+                required
+                onChange={(e)=>onChange(e)}
+                bg={"green.50"} borderRadius={"40px"}/>
             </Box>
 
             <Box textAlign={"center"}>
-              <Button display={"inline-block"} mt={"24px"} color={"white"} bg={"green.600"} borderRadius={"50px"} height={"60px"} width={"200px"}>
+              <Button onClick={(e)=>onSubmit(e)} display={"inline-block"} mt={"24px"} color={"white"} bg={"green.600"} borderRadius={"50px"} height={"60px"} width={"200px"}>
                 SIGN UP
               </Button>
             </Box>
           </Box>
         </Box>
       </Box>
-
-
     </>
   )
-}
+  }
 
-export default signup
+
+export default Signup
