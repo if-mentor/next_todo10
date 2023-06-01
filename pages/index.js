df18be44b55fa9ede830d594d4b5aeb955b544a2
masac <msktjm98@gmail.com>
2023-06-01 14:59:01 +0900

    index もとに

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,10 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      {/* 面倒なので一時的に変更 */}
-      {/* <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <h1>Hello, Next.js HELLO!!</h1>
-      </div> */}
-      <Test />
+      </div>
+      
     </>
   );
 }
