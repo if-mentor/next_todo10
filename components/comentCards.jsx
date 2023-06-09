8d64d33293337008a88ead22cdf6d054a862ee09
masac <msktjm98@gmail.com>
2023-06-09 21:07:34 +0900

    コメントをmap関数で表示

--- components/comentCards.jsx
+++ components/comentCards.jsx
@@ -7,79 +7,43 @@ import { VStack, Box, HStack } from "@chakra-ui/react";
 import * as styles from "../styles/comentCards.module.css";
 
 export const ComentCards = () => {
+  const comments = [
+    {
+      id: "111111111",
+      name: "aaa",
+      date: "2022/01/02",
+      comment: "おはよう",
+    },
+    {
+      id: "666666",
+      name: "bbb",
+      date: "2022 / 01 / 01",
+      comment: "おはヨーグルト",
+    },
+    {
+      id: "77777",
+      name: "ccc",
+      date: "2022 / 01 / 01",
+      comment: "おはこんばんにちは",
+    },
+  ]
   return (
-    <VStack
-      // w={"472px"}
-      // h={"464px"}
-      display="grid"
-      height="480px"
-      overflow="scroll"
-    >
-      {/* 1*/}
-      <Box className={styles.commentFrame}>
-        <HStack className={styles.commentTopFrame}>
-          <Box className={styles.commentContent} left={"24px"}>
-            ジョン
+    <VStack display="grid" height="480px" overflow="scroll">
+      {comments.map((comment) => {
+        return (
+          <Box className={styles.commentFrame} key={comment.id}>
+            <HStack className={styles.commentTopFrame}>
+              <Box className={styles.commentContent} ml='10'>
+                {comment.name}
+              </Box>
+              <Box className={styles.commentContent}>
+                {comment.date}
+              </Box>
+            </HStack>
+            <Box>{comment.comment}</Box>
           </Box>
-          <Box className={styles.commentContent} left={"361px"}>
-            2022/01/01
-          </Box>
-        </HStack>
-        <Box>2日後までに完了お願い致します。</Box>
-      </Box>
-
-      {/* 2*/}
-      <Box className={styles.commentFrame} top={"120px"}>
-        <HStack className={styles.commentTopFrame}>
-          <Box className={styles.commentContent} left={"24px"}>
-            ジョン
-          </Box>
-          <Box className={styles.commentContent} left={"361px"}>
-            2022/01/01
-          </Box>
-        </HStack>
-        <Box>内容確認致しました。修正点メールしましたのでご確認ください。</Box>
-      </Box>
-
-      {/* 3*/}
-      <Box className={styles.commentFrame} top={"240px"}>
-        <HStack className={styles.commentTopFrame}>
-          <Box className={styles.commentContent} left={"24px"}>
-            ジョン
-          </Box>
-          <Box className={styles.commentContent} left={"361px"}>
-            2022/01/01
-          </Box>
-        </HStack>
-        <Box>2日後までに完了お願い致します。</Box>
-      </Box>
-      
-      {/* 4*/}
-      <Box className={styles.commentFrame} top={"360px"}>
-        <HStack className={styles.commentTopFrame}>
-          <Box className={styles.commentContent} left={"24px"}>
-            ジョン
-          </Box>
-          <Box className={styles.commentContent} left={"361px"}>
-            2022/01/01
-          </Box>
-        </HStack>
-        <Box>2日後までに完了お願い致します。</Box>
-      </Box>
-
-      <Box className={styles.commentFrame} top={"360px"}>
-        <HStack className={styles.commentTopFrame}>
-          <Box className={styles.commentContent} left={"24px"}>
-            ジョン
-          </Box>
-          <Box className={styles.commentContent} left={"361px"}>
-            2022/01/01
-          </Box>
-        </HStack>
-        <Box>2日後までに完了お願い致します。</Box>
-      </Box>
-
-      
+        );
+      })}
     </VStack>
   );
 };
