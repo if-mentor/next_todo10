2faa45206d55698c7d87f92db871073d7b947780
masac <msktjm98@gmail.com>
2023-06-10 19:19:44 +0900

    firebaseからの読み取り

--- components/comentCards.jsx
+++ components/comentCards.jsx
@@ -1,43 +1,57 @@
 /*
 styleはtop以外同じなので複製できるようにしたい
 mapで処理したい
+idに対応してコメントが出るようにしたい
 */
 
+import { useEffect, useState } from "react";
 import { VStack, Box, HStack } from "@chakra-ui/react";
 import * as styles from "../styles/comentCards.module.css";
 
+import { db } from "@/libs/firebase";
+import { collection, onSnapshot } from "firebase/firestore";
+
+import { DateDisplay } from "./DateDisplay";
+
+
 export const ComentCards = () => {
-  const comments = [
-    {
-      id: "111111111",
-      name: "aaa",
-      date: "2022/01/02",
-      comment: "おはよう",
-    },
-    {
-      id: "666666",
-      name: "bbb",
-      date: "2022 / 01 / 01",
-      comment: "おはヨーグルト",
-    },
-    {
-      id: "77777",
-      name: "ccc",
-      date: "2022 / 01 / 01",
-      comment: "おはこんばんにちは",
-    },
-  ]
+  //posts -> commentlist -> comments -> {id,comment...}
+  const [commentList, setCommentlist] = useState([])
+
+  //firebaseからの読み取り
+  const readCommentData = async () => {
+    const commentData = collection(db, "posts");
+    onSnapshot(commentData, (snapshot) => {
+      const newCommentsList = [];
+      snapshot.docs.map((doc) => {
+        const comments = {
+          id: doc.data().todoId,
+          name: doc.data().name,
+          comment: doc.data().comment,
+          date: doc.data().date.toDate(),
+        };
+        newCommentsList.push({ ...comments });
+      });
+      setCommentlist(newCommentsList);
+    });
+  };
+
+  useEffect(() => {
+    readCommentData();
+  }, []);
+  
   return (
     <VStack display="grid" height="480px" overflow="scroll">
-      {comments.map((comment) => {
+
+      {commentList.map((comment) => {
         return (
           <Box className={styles.commentFrame} key={comment.id}>
             <HStack className={styles.commentTopFrame}>
-              <Box className={styles.commentContent} ml='10'>
+              <Box className={styles.commentContent} ml="10">
                 {comment.name}
               </Box>
               <Box className={styles.commentContent}>
-                {comment.date}
+                <DateDisplay date={comment.date} />
               </Box>
             </HStack>
             <Box>{comment.comment}</Box>
