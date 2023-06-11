dac03e63575a99f704d128476a77f4782eabf487
masac <msktjm98@gmail.com>
2023-06-11 23:51:19 +0900

    idによる選別

--- components/comentCards.jsx
+++ components/comentCards.jsx
@@ -7,18 +7,15 @@ idに対応してコメントが出るようにしたい
 import { useEffect, useState } from "react";
 import { VStack, Box, HStack } from "@chakra-ui/react";
 import * as styles from "../styles/comentCards.module.css";
-
 import { db } from "@/libs/firebase";
 import { collection, onSnapshot } from "firebase/firestore";
-
 import { DateDisplay } from "./DateDisplay";
-
+import { useRouter } from "next/router";
 
 export const ComentCards = () => {
-  //posts -> commentlist -> comments -> {id,comment...}
-  const [commentList, setCommentlist] = useState([])
-
   //firebaseからの読み取り
+  //posts -> commentlist -> comments -> {id,comment...}
+  const [commentList, setCommentlist] = useState([]);
   const readCommentData = async () => {
     const commentData = collection(db, "posts");
     onSnapshot(commentData, (snapshot) => {
@@ -39,25 +36,33 @@ export const ComentCards = () => {
   useEffect(() => {
     readCommentData();
   }, []);
-  
+
+  //idによる選別
+  const router = useRouter();
+  const id  = router.query.id;
+  const getTodoById = (id) => {
+    return commentList.find((comments) => comments.id === id);
+  };
+
+  const comments = getTodoById(id);
+  const name = comments ? comments.name : "";
+  const comment = comments ? comments.comment : "";
+  const date = comments ? comments.date : "";
+  // console.log(comments);
+
   return (
     <VStack display="grid" height="480px" overflow="scroll">
-
-      {commentList.map((comment) => {
-        return (
-          <Box className={styles.commentFrame} key={comment.id}>
-            <HStack className={styles.commentTopFrame}>
-              <Box className={styles.commentContent} ml="10">
-                {comment.name}
-              </Box>
-              <Box className={styles.commentContent}>
-                <DateDisplay date={comment.date} />
-              </Box>
-            </HStack>
-            <Box>{comment.comment}</Box>
+      <Box className={styles.commentFrame} key={id}>
+        <HStack className={styles.commentTopFrame}>
+          <Box className={styles.commentContent} ml="10">
+            {name}
           </Box>
-        );
-      })}
+          <Box className={styles.commentContent}>
+            <DateDisplay date={date} />
+          </Box>
+        </HStack>
+        <Box>{comment}</Box>
+      </Box>
     </VStack>
   );
 };
