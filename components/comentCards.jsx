71937efd05d63fcec4afc6ecd24a36ed064dbff5
masac <msktjm98@gmail.com>
2023-06-14 05:44:43 +0900

    コメント表示できるようになった

--- components/comentCards.jsx
+++ components/comentCards.jsx
@@ -1,68 +1,106 @@
-/*
-styleはtop以外同じなので複製できるようにしたい
-mapで処理したい
-idに対応してコメントが出るようにしたい
-*/
-
 import { useEffect, useState } from "react";
-import { VStack, Box, HStack } from "@chakra-ui/react";
+import { VStack, Box, HStack, Text } from "@chakra-ui/react";
 import * as styles from "../styles/comentCards.module.css";
 import { db } from "@/libs/firebase";
-import { collection, onSnapshot } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  collection,
+  where,
+} from "firebase/firestore";
 import { DateDisplay } from "./DateDisplay";
-import { useRouter } from "next/router";
+import { useGettingId } from "@/hooks/useGettingId";
 
 export const ComentCards = () => {
   //firebaseからの読み取り
-  //posts -> commentlist -> comments -> {id,comment...}
+  //posts -> commentlist -> {id,comment...}
   const [commentList, setCommentlist] = useState([]);
-  const readCommentData = async () => {
-    const commentData = collection(db, "posts");
-    onSnapshot(commentData, (snapshot) => {
-      const newCommentsList = [];
-      snapshot.docs.map((doc) => {
-        const comments = {
-          id: doc.data().todoId,
-          name: doc.data().name,
-          comment: doc.data().comment,
-          date: doc.data().date.toDate(),
-        };
-        newCommentsList.push({ ...comments });
-      });
-      setCommentlist(newCommentsList);
-    });
-  };
+  // const readCommentData = async () => {
+  //   const commentData = collection(db, "posts");
+  //   onSnapshot(commentData, (snapshot) => {
+  //     const newCommentsList = [];
+  //     snapshot.docs.map((doc) => {
+  //       newCommentsList.push({
+  //         todoId: doc.data().todoId,
+  //         name: doc.data().name,
+  //         comment: doc.data().comment,
+  //         date: doc.data().date.toDate(),
+  //       });
+  //     });
+  //     setCommentlist(newCommentsList);
+  //   });
+  //   console.log(commentData);
+  // };
 
-  useEffect(() => {
-    readCommentData();
-  }, []);
+  // useEffect(() => {
+  //   readCommentData();
+  // }, []);
 
   //idによる選別
-  const router = useRouter();
-  const id  = router.query.id;
-  const getTodoById = (id) => {
-    return commentList.find((comments) => comments.id === id);
-  };
+  const { todoId } = useGettingId();
 
-  const comments = getTodoById(id);
-  const name = comments ? comments.name : "";
-  const comment = comments ? comments.comment : "";
-  const date = comments ? comments.date : "";
-  // console.log(comments);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const docRef = doc(db, "posts", todoId);
+  //     const docSnap = await getDoc(docRef);
+  //     if (docSnap.exists()) {
+  //       const commentData = docSnap.data();
+  //       setCommentlist([commentData]);
+  //     } else {
+  //       setCommentlist([]);
+  //     }
+  //   };
+  //   if (todoId) {
+  //     fetchData();
+  //   }
+  //   console.log(commentList);
+  // }, [todoId]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(
+        query(collection(db, "posts"), where("todoId", "==", todoId))
+      );
+      const newCommentsList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        todoId: doc.data().todoId,
+        name: doc.data().name,
+        comment: doc.data().comment,
+        // date: doc.data().date.toDate(),
+      }));
+      setCommentlist(newCommentsList);
+    };
+
+    if (todoId) {
+      fetchData();
+    }
+  }, [todoId]);
+
+  console.log(commentList);
 
   return (
-    <VStack display="grid" height="480px" overflow="scroll">
-      <Box className={styles.commentFrame} key={id}>
-        <HStack className={styles.commentTopFrame}>
-          <Box className={styles.commentContent} ml="10">
-            {name}
-          </Box>
-          <Box className={styles.commentContent}>
-            <DateDisplay date={date} />
-          </Box>
-        </HStack>
-        <Box>{comment}</Box>
-      </Box>
-    </VStack>
+    <>
+      {commentList.length === 0 ? (
+        <Text>コメントはまだありません</Text>
+      ) : (
+        <VStack display="grid" height="480px" overflow="scroll">
+          {commentList.map((comments) => (
+            <Box className={styles.commentFrame} key={comments.id}>
+              <HStack className={styles.commentTopFrame}>
+                <Box className={styles.commentContent} ml="10">
+                  {comments.name}
+                </Box>
+                <Box className={styles.commentContent}>
+                  <DateDisplay date={comments.date} />
+                </Box>
+              </HStack>
+              <Box>{comments.comment}</Box>
+            </Box>
+          ))}
+        </VStack>
+      )}
+    </>
   );
 };
