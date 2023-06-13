d6b2498d1ed24ff0c1421ab183aaf5c60e06c661
masac <msktjm98@gmail.com>
2023-06-14 05:11:00 +0900

    名前をを分かりやすく変更

--- hooks/useGettingId.jsx
+++ hooks/useGettingId.jsx
@@ -1,18 +1,17 @@
 import { useRouter } from "next/router";
-import { useTodo } from "./useTodo";
 
 export const useGettingId = () => {
-  const { todos } = useTodo();
 
   //idの取得
   const router = useRouter();
-  const { id } = router.query;
+  const todoId  = router.query.id;
 
   //取得したidに紐づいたtodoを呼び出す
-  const getTodoById = (id) => {
-    return todos.find((todo) => todo.id === id);
-  };
-  const todo = getTodoById(id);
+  //全件取得はレンダリングが多すぎた
+  // const getTodoById = (id) => {
+  //   return todos.find((todo) => todo.id === id);
+  // };
+  // const todo = getTodoById(id);
 
-  return { id, todo };
+  return { todoId };
 };
